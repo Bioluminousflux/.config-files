608ef6e1c3309fd7471ef755bdcc923e95072bc6
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 08:22:30 -0400

    Update beta-conky.sh

--- bugtraq/scripts/conky/beta-conky.sh
+++ bugtraq/scripts/conky/beta-conky.sh
@@ -1,3 +1,3 @@
-               conky -c /bugtraq/scripts/conky/conexiones.conf
+               conky -c /bugtraq/scripts/conky/connections.conf
                conky -c /bugtraq/scripts/conky/proxys.conf
                sudo conky -c /bugtraq/scripts/conky/iptables.conf
