57fba0ad35c6e46f0a3ba6a448ec8939cd270bde
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 00:18:32 -0400

    Update conky4.sh

--- bugtraq/scripts/conky/ON-OFF/scripts/conky4.sh
+++ bugtraq/scripts/conky/ON-OFF/scripts/conky4.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/ksh
 #------------------------------------------------------------------------------------
-# - Nombre: ./conky4.sh 
+# - Name: ./conky4.sh 
 # - Version: 1.0
-# - Fecha: 26/11/2011
-# - Creador: Bugtraq-Team
-# - descripción: Este script comprueba el estado de los servicios de bugtraq
-#     en el conky
+# - Date: Novmeber 26th, 2011
+# - Creator: Bugtraq-Team
+# - Description: This script checks the status of the bugtraq services in the conky
 #------------------------------------------------------------------------------------
 ###################################################################################################
 #Apache2
