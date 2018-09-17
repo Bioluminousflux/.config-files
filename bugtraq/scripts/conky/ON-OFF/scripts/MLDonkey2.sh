14449814a545bac244468d3793155edaebb7d570
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 00:05:46 -0400

    Update MLDonkey2.sh

--- bugtraq/scripts/conky/ON-OFF/scripts/MLDonkey2.sh
+++ bugtraq/scripts/conky/ON-OFF/scripts/MLDonkey2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/ksh
 #------------------------------------------------------------------------------------
-# - Name: ./MLDonkey.sh 
+# - Name: ./MLDonkey2.sh 
 # - Version: 1.0
 # - Date: November 26th, 2011
 # - Creator: Bugtraq-Team
