e8799309fb23e09b3f47d5020174b7900b76e07d
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-16 23:58:44 -0400

    Update apache2.sh

--- bugtraq/scripts/conky/ON-OFF/scripts/apache2.sh
+++ bugtraq/scripts/conky/ON-OFF/scripts/apache2.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/ksh
 #------------------------------------------------------------------------------------
-# - Nombre: ./Nessus.sh 
-# - Version: 2.0
-# - Fecha: 26/11/2011
-# - Creador: Bugtraq Team
-# - descripción: Este script comprueba el estado del apache mostrado en el conky
+# - Name: ./apache2.sh 
+# - Version: 1.0
+# - Date: November 26th, 2011
+# - Creator: Bugtraq-Team
+# - Description: This script checks the status of Apache2 in the conky
 #------------------------------------------------------------------------------------
-
 ###################################################################################################
 #Apache2
 if /etc/init.d/apache2 status | grep "NOT running" > /dev/null 2>&1
