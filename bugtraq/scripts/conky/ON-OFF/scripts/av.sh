a22f3b72595051f9aa024936613fd3f579cf714a
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 00:09:15 -0400

    Update av.sh

--- bugtraq/scripts/conky/ON-OFF/scripts/av.sh
+++ bugtraq/scripts/conky/ON-OFF/scripts/av.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/ksh
 #------------------------------------------------------------------------------------
-# - Nombre: ./av.sh 
+# - Name: ./av.sh 
 # - Version: 1.0
-# - Fecha: 26/11/2011
-# - Creador: Bugtraq Team
-# - descripción: Este script compruba el estadado de  los antivirus en el conky
+# - Date: November 26th, 2011
+# - Creator: Bugtraq-Team
+# - Description: This script checks the status of the Antivirus in the conky
 #------------------------------------------------------------------------------------
 ###################################################################################################
 #AVG
