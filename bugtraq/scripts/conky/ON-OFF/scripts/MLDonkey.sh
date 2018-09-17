3f0cd2c65437ad17e2c7ca093d388b04813f34cf
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-16 23:49:38 -0400

    Update MLDonkey.sh

--- bugtraq/scripts/conky/ON-OFF/scripts/MLDonkey.sh
+++ bugtraq/scripts/conky/ON-OFF/scripts/MLDonkey.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/ksh
 #------------------------------------------------------------------------------------
-# - Nombre: ./MLDonkey.sh 
+# - Name: ./MLDonkey.sh 
 # - Version: 1.0
-# - Fecha: 26/11/2011
-# - Creador: Bugtraq-Team
-# - descripción: Este script comprueba el estado del MLDonkey en el conky
+# - Date: November 26th, 2011
+# - Creator: Bugtraq-Team
+# - Description: This script checks the status of MLDonkey in the conky
 #------------------------------------------------------------------------------------
 ###################################################################################################
 #MLDonkey
