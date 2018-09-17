e05e263f00fc10da320016970c85384a267e6f3e
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 00:19:39 -0400

    Update conky5.sh

--- bugtraq/scripts/conky/ON-OFF/scripts/conky5.sh
+++ bugtraq/scripts/conky/ON-OFF/scripts/conky5.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/ksh
 #------------------------------------------------------------------------------------
-# - Nombre: ./conky5.sh 
+# - Name: ./conky5.sh 
 # - Version: 1.0
-# - Fecha: 26/11/2011
-# - Creador: Bugtraq-Team
-# - descripción: Este script comprueba el estado de las bases de
-#    de datos Postgres y mysql en el conky
+# - Date: Novmeber 26th, 2011
+# - Creator: Bugtraq-Team
+# - Description: This script checks the status of the Postgres and mysql databases in the conky
 #------------------------------------------------------------------------------------
 #Potsgres
 if /etc/init.d/postgresql-8.4 status | grep "8.4/main" > /dev/null 2>&1
