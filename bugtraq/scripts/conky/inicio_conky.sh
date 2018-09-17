df6c609e6a012aa0185d3b1b2c57877fa5daacb5
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 08:16:01 -0400

    Update inicio_conky.sh

--- bugtraq/scripts/conky/inicio_conky.sh
+++ bugtraq/scripts/conky/inicio_conky.sh
@@ -3,7 +3,7 @@ cat /proc/cpuinfo | grep "processor" > cpu.txt
 if cat cpu.txt | grep 4 > /dev/null 2>&1
 	then
 		conky -c /bugtraq/scripts/conky/.conkyrc
-	#	conky -c /bugtraq/scripts/conky/conexiones.conf
+	#	conky -c /bugtraq/scripts/conky/connections.conf
 	#	conky -c /bugtraq/scripts/conky/proxys.conf
 	#	sudo conky -c /bugtraq/scripts/conky/iptables.conf
 	else
