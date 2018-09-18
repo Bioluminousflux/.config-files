4632830e49288ea077816c4134c0005c36ddaa3b
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 23:24:49 -0400

    Update nod1.sh

--- bugtraq/scripts/conky/scripts/nod1.sh
+++ bugtraq/scripts/conky/scripts/nod1.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/ksh
 #------------------------------------------------------------------------------------
-# - Nombre: ./nod1.sh 
+# - Name: ./nod1.sh 
 # - Version: 1.0
-# - Fecha: 26/11/2011
-# - Creador: Bugtraq-Team
-# - descripción: Este script comprueba el estado del nod32 en el conky
+# - Date: November 26th, 2011
+# - Creator: Bugtraq-Team
+# - Description: This checks the status of Nod 32 in the conky
 #------------------------------------------------------------------------------------
-#####################################################
+#####################################################################################
 #Eset Nod 32
 ps -ef | grep "eset" > /tmp/tmp.txt
 if grep -e "/opt/eset/esets/sbin/esets_scan" -e  "/opt/eset/esets/sbin/esets_daemon" /tmp/tmp.txt > /dev/null 2>&1;
