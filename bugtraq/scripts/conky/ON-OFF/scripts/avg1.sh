233ae4297aac884ce916a0ed36fb8c75328289bf
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 22:34:07 -0400

    Update avg1.sh

--- bugtraq/scripts/conky/ON-OFF/scripts/avg1.sh
+++ bugtraq/scripts/conky/ON-OFF/scripts/avg1.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/ksh
 #------------------------------------------------------------------------------------
-# - Nombre: ./avg1.sh 
+# - Name: ./avg1.sh 
 # - Version: 1.0
-# - Fecha: 26/11/2011
-# - Creador: Bugtraq-Team
-# - descripción: Este script comprueba el estado del AVG en el conky
-###################################################################################################
+# - Date: November 26th, 2011
+# - Creator: Bugtraq-Team
+# - Description: This script checks the status of AVG in the Conky
+#------------------------------------------------------------------------------------
+#####################################################################################
 #AVG
 ps -ef | grep "avg" > /tmp/tmp.txt
 if grep "/opt/avg/av/bin/avgscand" /tmp/tmp.txt > /dev/null 2>&1;
