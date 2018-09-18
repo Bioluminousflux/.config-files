033aa9caba534b1ef4296e5fc2be442099aba935
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 23:12:39 -0400

    Update clam1.sh

--- bugtraq/scripts/conky/scripts/clam1.sh
+++ bugtraq/scripts/conky/scripts/clam1.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/ksh
 #------------------------------------------------------------------------------------
-# - Nombre: ./clam1.sh 
+# - Name: ./clam1.sh 
 # - Version: 1.0
-# - Fecha: 26/11/2011
-# - Creador: Bugtraq-Team
-# - descripción: Este script comprueba el estado del clamav en el conky
+# - Date: November 26th, 2011
+# - Creator: Bugtraq-Team
+# - Description: This script checks the status of ClamAV in the conky
 #------------------------------------------------------------------------------------
-#####################################################
+#####################################################################################
 #ClamAV
 ps -ef | grep "clam" > /tmp/tmp.txt
 if grep -e "/usr/bin/clamscan" -e "/usr/bin/freshclam" /tmp/tmp.txt > /dev/null 2>&1;
