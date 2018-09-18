cb79586c92a7fe3b82706d10066b0bd55dce7d43
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 22:40:04 -0400

    Update osiris.sh

--- bugtraq/scripts/conky/ON-OFF/scripts/osiris.sh
+++ bugtraq/scripts/conky/ON-OFF/scripts/osiris.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/ksh
 #------------------------------------------------------------------------------------
-# - Nombre: ./osiris.sh 
+# - Name: ./osiris.sh 
 # - Version: 1.0
-# - Fecha: 26/11/2011
-# - Creador: Bugtraq-Team
-# - descripción: Este script comprueba el estado de osiris en el conky
+# - Date: November 26th, 2011
+# - Creator: Bugtraq-Team
+# - Description: This script checks the status of Osiris in the Conky
 #------------------------------------------------------------------------------------
-#####################################################
-#osiris
+#####################################################################################
+#Osiris
 ps -ef | grep "osiris" > /tmp/tmp.txt
 if grep "/usr/bin/osiris-sps" /tmp/tmp.txt > /dev/null 2>&1;
 	then
