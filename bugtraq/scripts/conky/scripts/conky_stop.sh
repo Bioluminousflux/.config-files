a2c9ba2fed1eb7268d9c3a9c6fc6221a8afc29ad
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 23:14:08 -0400

    Update conky_stop.sh

--- bugtraq/scripts/conky/scripts/conky_stop.sh
+++ bugtraq/scripts/conky/scripts/conky_stop.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/ksh
-
+#------------------------------------------------------------------------------------
+# - Name: ./conky_stop.sh 
+# - Version: 1.0
+# - Date: November 26th, 2011
+# - Creator: Bugtraq-Team
+# - Description: This script kills/stops the Conky
+#------------------------------------------------------------------------------------
+#####################################################################################
+#Conky
 PROC=`ps -ef | grep conky | head -n1 | awk '{print $2}'`
 kill -9 $PROC
 pkill conky
