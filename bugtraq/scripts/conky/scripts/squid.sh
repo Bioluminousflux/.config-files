eea01158118c1f5b0ba23a5de73338a52dcb49a6
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 23:32:50 -0400

    Update squid.sh

--- bugtraq/scripts/conky/scripts/squid.sh
+++ bugtraq/scripts/conky/scripts/squid.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/ksh
 #------------------------------------------------------------------------------------
-# - Nombre: ./squid.sh 
+# - Name: ./squid.sh 
 # - Version: 1.0
-# - Fecha: 26/11/2011
-# - Creador: Bugtraq-Team
-# - descripción: Este script comprueba el estado de privoxy en el conky
+# - Date: November 26th, 2011
+# - Creator: Bugtraq-Team
+# - Description: This checks the status of Squid in the conky
 #------------------------------------------------------------------------------------
-###################################################################################################
-#squid
+#####################################################################################
+#Squid
 if ps -ef | grep "squid3" | grep "/usr/sbin" > /dev/null 2>&1
 
 	then
