15b16d360f0fe662986318b2e92a25dc2d3c9c4b
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 23:31:58 -0400

    Update set_stop.sh

--- bugtraq/scripts/conky/scripts/set_stop.sh
+++ bugtraq/scripts/conky/scripts/set_stop.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/ksh
-
+#------------------------------------------------------------------------------------
+# - Name: ./set_stop.sh 
+# - Version: 1.0
+# - Date: November 26th, 2011
+# - Creator: Bugtraq-Team
+# - Description: This stops the Set-Web service
+#------------------------------------------------------------------------------------
+#####################################################################################
+#Set-Web
 PROC=`ps -ef | grep "/pentesting/set" | grep -v "grep" | awk '{print $2}'`
 kill -9 $PROC
 PORC2=`ps -ef | grep set_start | grep sudo | grep -v "grep" | awk '{print $2}'`
