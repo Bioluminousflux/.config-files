bbc6d10f794ff3afb36589dddd6411158420b7c7
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 23:16:27 -0400

    Update evilgrade_start.sh

--- bugtraq/scripts/conky/scripts/evilgrade_start.sh
+++ bugtraq/scripts/conky/scripts/evilgrade_start.sh
@@ -1,2 +1,12 @@
+#!/usr/bin/ksh
+#------------------------------------------------------------------------------------
+# - Name: ./evilgrade.sh 
+# - Version: 1.0
+# - Date: November 26th, 2011
+# - Creator: Bugtraq-Team
+# - Description: This script starts the Evilgrade service
+#------------------------------------------------------------------------------------
+#####################################################################################
+#Evilgrade
 cd /bugtraq/tools/pentesting/isr-evilgrade/
 sudo ./evilgrade
