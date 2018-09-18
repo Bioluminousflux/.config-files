efafcbf49b8bd52c7f68f1d142ed68ffb2de252c
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 23:08:01 -0400

    Update avira1.sh

--- bugtraq/scripts/conky/scripts/avira1.sh
+++ bugtraq/scripts/conky/scripts/avira1.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/ksh
 #------------------------------------------------------------------------------------
-# - Nombre: ./avira1.sh 
+# - Name: ./avira1.sh 
 # - Version: 1.0
-# - Fecha: 26/11/2011
-# - Creador: Bugtraq-Team
-# - descripción: Este script comprueba el estado de avira en el conky
-#####################################################
-#AVAST
+# - Date: November 26th, 2011
+# - Creator: Bugtraq-Team
+# - Description: This script checks the status of Avira in the Conky
+#------------------------------------------------------------------------------------
+###################################################################################################
+#Avira
 ps -ef | grep "guard" > /tmp/tmp.txt
 if grep -e "/guard/scancl" -e "/guard/avupdate-guard" /tmp/tmp.txt > /dev/null 2>&1;
 	then
