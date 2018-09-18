047a66475b4e418723545cefa0e9dcfd01b1f88e
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 22:38:29 -0400

    Update beef.sh

--- bugtraq/scripts/conky/ON-OFF/scripts/beef.sh
+++ bugtraq/scripts/conky/ON-OFF/scripts/beef.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/ksh
 #------------------------------------------------------------------------------------
-# - Nombre: ./beef.sh 
+# - Name: ./beef.sh 
 # - Version: 1.0
-# - Fecha: 26/11/2011
-# - Creador: Bugtraq-Team
-# - descripción: Este script comprueba el estado del beef en el conky
-###################################################################################################
+# - Date: November 26th, 2011
+# - Creator: Bugtraq-Team
+# - Description: This script checks the status of Beef in the Conky
+#------------------------------------------------------------------------------------
+#####################################################################################
 #Beef
 ps -ef | grep "beef" > /tmp/tmp.txt
 if grep "ruby beef" /tmp/tmp.txt > /dev/null 2>&1;
