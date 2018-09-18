46d77e3980cdd686ba30dcc1185a894e3f1471b6
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 23:16:58 -0400

    Update evilgrade_stop.sh

--- bugtraq/scripts/conky/scripts/evilgrade_stop.sh
+++ bugtraq/scripts/conky/scripts/evilgrade_stop.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/ksh
-
+#------------------------------------------------------------------------------------
+# - Name: ./evilgrade_stop.sh 
+# - Version: 1.0
+# - Date: November 26th, 2011
+# - Creator: Bugtraq-Team
+# - Description: This script stops/kills the Evilgrade service
+#------------------------------------------------------------------------------------
+#####################################################################################
+#Evilgrade
 PROC=`ps -ef | grep evilgrade | grep perl | awk '{print $2}' | head -n1`
 sudo kill -9 $PROC
