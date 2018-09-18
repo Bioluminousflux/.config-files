7be22df783a43c7bbcbe3dc356ae1910a1b20687
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 22:49:51 -0400

    Update msf.sh

--- bugtraq/scripts/conky/ON-OFF/scripts/msf.sh
+++ bugtraq/scripts/conky/ON-OFF/scripts/msf.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/ksh
 #------------------------------------------------------------------------------------
-# - Nombre: ./metasploit.sh 
+# - Name: ./msf.sh 
 # - Version: 1.0
-# - Fecha: 26/11/2011
-# - Creador: Bugtraq-Team
-# - descripción: Este script comprueba el estado del metsasploit en el conky
+# - Date: November 26th, 2011
+# - Creator: Bugtraq-Team
+# - Description: This script checks the status of Metasploit in the Conky
 #------------------------------------------------------------------------------------
-###################################################################################################
+#####################################################################################
 #Metasploit
 ps -ef | grep "metasploit" > /tmp/tmp.txt
 if grep "/opt/metasploit" /tmp/tmp.txt > /dev/null 2>&1;
