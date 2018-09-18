6819126da028f3f14225519ce5c610d915c20e66
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 23:28:05 -0400

    Update openvas_stop.sh

--- bugtraq/scripts/conky/scripts/openvas_stop.sh
+++ bugtraq/scripts/conky/scripts/openvas_stop.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-
+#------------------------------------------------------------------------------------
+# - Name: ./openvas_stop.sh 
+# - Version: 1.0
+# - Date: November 26th, 2011
+# - Creator: Bugtraq-Team
+# - Description: This stops the OpenVAS service
+#------------------------------------------------------------------------------------
+#####################################################################################
+#OpenVAS
 sudo killall openvassd
 sleep 4 4 4 4 
 sudo /etc/init.d/openvas-scanner stop
