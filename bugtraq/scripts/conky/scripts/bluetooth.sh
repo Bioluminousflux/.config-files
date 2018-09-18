feabdabc5bb07e2001808a6cd496155c2d3cbf05
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 23:11:45 -0400

    Update bluetooth.sh

--- bugtraq/scripts/conky/scripts/bluetooth.sh
+++ bugtraq/scripts/conky/scripts/bluetooth.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/ksh
 #------------------------------------------------------------------------------------
-# - Nombre: ./bluetooth.sh 
+# - Name: ./bluetooth.sh 
 # - Version: 1.0
-# - Fecha: 26/11/2011
-# - Creador: Bugtraq-Team
-# - descripción: Este script comprueba el estado del bluetooth en el conky
+# - Date: November 26th, 2011
+# - Creator: Bugtraq-Team
+# - Description: This script checks the status of Bluetooth in the conky
 #------------------------------------------------------------------------------------
-###################################################################################################
+#####################################################################################
 #Bluetooth
 if /etc/init.d/bluetooth status | grep "is not running" > /dev/null 2>&1
 
