3929104035b6bf6cf0265afd85a48bbb6fef6e94
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 23:35:11 -0400

    Update wifizoo_start.sh

--- bugtraq/scripts/conky/scripts/wifizoo_start.sh
+++ bugtraq/scripts/conky/scripts/wifizoo_start.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/ksh
-
+#------------------------------------------------------------------------------------
+# - Name: ./wifizoo_start.sh 
+# - Version: 1.0
+# - Date: November 26th, 2011
+# - Creator: Bugtraq-Team
+# - Description: This starts the Wifizoo Service
+#------------------------------------------------------------------------------------
+#####################################################################################
+#Wifizoo
 cd /bugtraq/tools/communication/802.11/wireless/wifizoo
 gnome-terminal -e "python wifizoo.py -i wlan0"
