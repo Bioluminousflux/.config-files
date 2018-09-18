80ea773f7d606341cd787d832ee35c2343a9f4a7
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 23:10:19 -0400

    Update beef_stop.sh

--- bugtraq/scripts/conky/scripts/beef_stop.sh
+++ bugtraq/scripts/conky/scripts/beef_stop.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/ksh
-
+#------------------------------------------------------------------------------------
+# - Name: ./beef.sh 
+# - Version: 1.0
+# - Date: November 26th, 2011
+# - Creator: Bugtraq-Team
+# - Description: This script stops/kills the Beef service
+#------------------------------------------------------------------------------------
+#####################################################################################
+#Beef
 PROC=`ps -ef | grep beef | grep ruby | awk '{print $2}'`
 sudo kill -9 $PROC
 sudo pkill beef
