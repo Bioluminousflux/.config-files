f09864ada1482f655f2c17e302f78b8e68e647c5
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 23:21:08 -0400

    Update iptables.sh

--- bugtraq/scripts/conky/scripts/iptables.sh
+++ bugtraq/scripts/conky/scripts/iptables.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/usr/bin/ksh
 #------------------------------------------------------------------------------------
-# - Nombre: ./apache.sh 
+# - Name: ./iptables.sh 
 # - Version: 1.0
-# - Fecha: 26/11/2011
-# - Creador: Bugtraq-Team
-# - descripción: Este script comprueba el estado del apache en el conky
+# - Date: November 26th, 2011
+# - Creator: Bugtraq-Team
+# - Description: This script displays your iptables configuration in the conky
 #------------------------------------------------------------------------------------
+#####################################################################################
 iptables --list > iptables.txt
 awk '/Chain INPUT/, /Chain FORWARD/' iptables.txt | awk {' if (NF != 0) print $0 '}  | sed 's/Chain FORWARD (policy ACCEPT)//g' | sed 's/Chain INPUT (policy ACCEPT)/${color yellow}+ Chain INPUT (policy ACCEPT)${color3}/g' | sed 's/Chain FORWARD (policy ACCEPT)/${color yellow}+ Chain FORWARD (policy ACCEPT)${color3}/g' | sed 's/Chain OUTPUT (policy ACCEPT)/${color yellow}+ Chain OUTPUT (policy ACCEPT)${color3}/g' | sed 's/ACCEPT /${color green}ACCEPT${color3}/g' | sed 's/REJECT/${color red}REJECT${color3}/g'
 cat iptables.txt | awk '/OUTPUT/,0' |  uniq | sed 's/Chain INPUT (policy ACCEPT)/${color yellow}+ Chain INPUT (policy ACCEPT)${color3}/g' | sed 's/Chain FORWARD (policy ACCEPT)/${color yellow}+ Chain FORWARD (policy ACCEPT)${color3}/g' | sed 's/Chain OUTPUT (policy ACCEPT)/${color yellow}+ Chain OUTPUT (policy ACCEPT)${color3}/g' | sed 's/ACCEPT /${color green}ACCEPT${color3}/g' | sed 's/REJECT/${color red}REJECT${color3}/g'
