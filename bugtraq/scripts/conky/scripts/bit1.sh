ccf1ef5471929dea40f82bdf17f226b9fc8d02a7
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 23:11:00 -0400

    Update bit1.sh

--- bugtraq/scripts/conky/scripts/bit1.sh
+++ bugtraq/scripts/conky/scripts/bit1.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/ksh
 #------------------------------------------------------------------------------------
-# - Nombre: ./bit1.sh 
+# - Name: ./bit1.sh 
 # - Version: 1.0
-# - Fecha: 26/11/2011
-# - Creador: Bugtraq-Team
-# - descripción: Este script comprueba el estado del bitdefender en el conky
-#####################################################
+# - Date: November 26th, 2011
+# - Creator: Bugtraq-Team
+# - Description: This script checks the status of Bitdefender in the conky
+#------------------------------------------------------------------------------------
+#####################################################################################
 #Bitdefender
 ps -ef | grep "bdscan" > /tmp/tmp.txt
 if grep "/opt/BitDefender-scanner/bin/bdscan" /tmp/tmp.txt > /dev/null 2>&1;
