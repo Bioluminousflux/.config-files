da21ce967264d97a16b2935b495bb86864b92f2c
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 23:35:38 -0400

    Update wifizoo_stop.sh

--- bugtraq/scripts/conky/scripts/wifizoo_stop.sh
+++ bugtraq/scripts/conky/scripts/wifizoo_stop.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/ksh
-
+#------------------------------------------------------------------------------------
+# - Name: ./wifizoo.sh 
+# - Version: 1.0
+# - Date: November 26th, 2011
+# - Creator: Bugtraq-Team
+# - Description: This stops the Wifizoo Service
+#------------------------------------------------------------------------------------
+#####################################################################################
+#Wifizoo
 PROC=`ps -ef | grep "wifizoo.py -i" | grep -v "grep" | grep "gnome" | awk '{print $2}'`
 kill -9 $PROC
