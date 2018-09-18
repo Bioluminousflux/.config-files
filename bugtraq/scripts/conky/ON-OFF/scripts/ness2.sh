a1ace37bde8e5408c4858761630952a4ba4c380f
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 22:47:24 -0400

    Update ness2.sh

--- bugtraq/scripts/conky/ON-OFF/scripts/ness2.sh
+++ bugtraq/scripts/conky/ON-OFF/scripts/ness2.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/ksh
 #------------------------------------------------------------------------------------
-# - Nombre: ./ness2.sh 
+# - Name: ./ness2.sh 
 # - Version: 1.0
-# - Fecha: 26/11/2011
-# - Creador: Bugtraq-Team
-# - descripción: Este script comprueba el estado del nessus en el conky
+# - Date: November 26th, 2011
+# - Creator: Bugtraq-Team
+# - Description: This script checks the status of Nessus in the Conky
 #------------------------------------------------------------------------------------
-##################################################################################################
+#####################################################################################
 #Nessus
 ps -ef | grep "nessus" > /tmp/tmp.txt
 if grep "nessusd -q" /tmp/tmp.txt > /dev/null 2>&1;
