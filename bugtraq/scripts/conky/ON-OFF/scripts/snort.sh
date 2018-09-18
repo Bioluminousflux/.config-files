7b6aa7c6ec9f35000a438059ff64e68adaceec49
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 22:30:14 -0400

    Update snort.sh

--- bugtraq/scripts/conky/ON-OFF/scripts/snort.sh
+++ bugtraq/scripts/conky/ON-OFF/scripts/snort.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/ksh
 #------------------------------------------------------------------------------------
-# - Nombre: ./snort.sh 
+# - Name: ./snort.sh 
 # - Version: 1.0
-# - Fecha: 26/11/2011
-# - Creador: Bugtraq-Team
-# - descripción: Este script comprueba el estado de snort en el conky
+# - Date: November 26th, 2011
+# - Creator: Bugtraq-Team
+# - Description: This script checks the status of Snort in the Conky
 #------------------------------------------------------------------------------------
-###################################################################################################
+#####################################################################################
 #Snort
 ps -ef | grep "snort" > /tmp/tmp.txt
 if grep "/usr/local/snort" /tmp/tmp.txt > /dev/null 2>&1;
