11ba7721b88a831534e57d886b56d143ef778597
Bioluminousflux <31056954+Bioluminousflux@users.noreply.github.com>
2018-09-17 23:31:26 -0400

    Update set-web.sh

--- bugtraq/scripts/conky/scripts/set-web.sh
+++ bugtraq/scripts/conky/scripts/set-web.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/ksh
 #------------------------------------------------------------------------------------
-# - Nombre: ./set-web.sh 
+# - Name: ./set-web.sh 
 # - Version: 1.0
-# - Fecha: 26/11/2011
-# - Creador: Bugtraq-Team
-# - descripción: Este script comprueba el estado de set-web en el conky
+# - Date: November 26th, 2011
+# - Creator: Bugtraq-Team
+# - Description: This checks the status of Set-Web in the conky
 #------------------------------------------------------------------------------------
-#####################################################
-#set-web
+#####################################################################################
+#Set-Web
 ps -ef | grep "set" > /tmp/tmp.txt
 if grep "/usr/bin/python ./set" /tmp/tmp.txt > /dev/null 2>&1;
 	then
